40998c5cca0323250c36b66c37d9af2781c85363
Rymbek Nazerke <nazerkerymbek3@gmail.com>
2026-02-01 17:50:53 +0500

    Create Artist.java

---
+++ src/model/Artist.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Artist {
+    private int id;
+    private String name;
+
+    public Artist(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+}
